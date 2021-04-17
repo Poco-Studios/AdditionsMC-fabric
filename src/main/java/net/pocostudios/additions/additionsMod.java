59716a38b627bd38ed1dc4662c35daa46ec7377e
AV3_08 <brandonjoash@gmail.com>
2021-04-17 10:43:58 +0800

    yay

--- src/main/java/net/pocostudios/additions/additionsMod.java
+++ src/main/java/net/pocostudios/additions/additionsMod.java
@@ -2,13 +2,13 @@ package net.pocostudios.additions;
 
 import net.fabricmc.api.ModInitializer;
 
-public class Init implements ModInitializer {
+public class additionsMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		System.out.println("Hello Fabric world!");
+		System.out.println("AdditionsMC is initializing. Have fun!");
 	}
 }
