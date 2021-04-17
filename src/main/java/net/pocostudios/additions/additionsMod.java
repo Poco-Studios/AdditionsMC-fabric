4e1d202781c3b96ffa3a59826733705cad59bdb4
AV3_08 <brandonjoash@gmail.com>
2021-04-17 11:04:51 +0800

    Autosave

--- src/main/java/net/pocostudios/additions/additionsMod.java
+++ src/main/java/net/pocostudios/additions/additionsMod.java
@@ -1,8 +1,17 @@
 package net.pocostudios.additions;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class additionsMod implements ModInitializer {
+
+	//test item
+	public static final Item TEST_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -10,5 +19,7 @@ public class additionsMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		System.out.println("AdditionsMC is initializing. Have fun!");
+
+		Registry.register(Registry.ITEM, new Identifier("additions", "test_item"), TEST_ITEM);
 	}
 }
