ad6559c1ad04ac5fe2f5fef1c0cf51973f6dddd7
AV3_08 <brandonjoash@gmail.com>
2021-04-20 18:16:08 +0800

    whee

--- src/main/java/net/pocostudios/additions/additionsMod.java
+++ src/main/java/net/pocostudios/additions/additionsMod.java
@@ -5,15 +5,20 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+
+import javax.tools.Tool;
 
 public class additionsMod implements ModInitializer {
 
 	public static final Item ALUMINIUM_INGOT = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+
+	public static ToolItem ALUMINIUM_SWORD = new SwordItem(AluminiumToolMaterial.INSTANCE, 7, +1.0F, new Item.Settings().group(ItemGroup.COMBAT));
+	public static ToolItem ALUMINIUM_SHOVEL = new ShovelItem(AluminiumToolMaterial.INSTANCE, 1.5F, +1.0F, new Item.Settings().group(ItemGroup.TOOLS));
+	//must figure out the Al axe and hoe (protected)
+
 
 	@Override
 	public void onInitialize() {
@@ -23,7 +28,9 @@ public class additionsMod implements ModInitializer {
 
 		System.out.println("AdditionsMC is initializing. Have fun!");
 
-		Registry.register(Registry.ITEM, new Identifier("additions", "aluminium_ingot"), ALUMINIUM_INGOT; //Al ingot
+		Registry.register(Registry.ITEM, new Identifier("additions", "aluminium_ingot"), ALUMINIUM_INGOT); //Al ingot
+
+
 
 	}
 }
