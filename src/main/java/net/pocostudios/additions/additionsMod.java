f23818a1b7b9360eadffad387f66f7c593b5a481
AV3_08 <brandonjoash@gmail.com>
2021-04-30 08:45:23 +0800

    Revert "ugh"
    This reverts commit 59c7886191032978d3248f7ec6b203103c7ae152.

--- src/main/java/net/pocostudios/additions/additionsMod.java
+++ src/main/java/net/pocostudios/additions/additionsMod.java
@@ -1,7 +1,5 @@
 package net.pocostudios.additions;
 
-
-import net.fabricmc.api.*;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
@@ -11,27 +9,15 @@ import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+import javax.tools.Tool;
 
 public class additionsMod implements ModInitializer {
 
-	//ores
-	/*private static ConfiguredFeature<?, ?> ORE_WOOL_OVERWORLD = Feature.ORE
-    .configure(new OreFeatureConfig(
-      OreFeatureConfig.Rules.BASE_STONE_OVERWORLD,
-      Blocks.ALUMINIUM_ORE.getDefaultState(),
-      5)) // vein size
-    .decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(
-      0,
-      0,
-      64)))
-    .spreadHorizontally()
-    .repeat(10); // number of veins per chunk*/
-
-
-	//Items
 	public static final Item ALUMINIUM_INGOT = new Item(new FabricItemSettings().group(ItemGroup.MISC));
-	public static final Item SILICON_INGOT = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
-	
+
+	public static ToolItem ALUMINIUM_SWORD = new SwordItem(AluminiumToolMaterial.INSTANCE, 7, +1.0F, new Item.Settings().group(ItemGroup.COMBAT));
+	public static ToolItem ALUMINIUM_SHOVEL = new ShovelItem(AluminiumToolMaterial.INSTANCE, 1.5F, +1.0F, new Item.Settings().group(ItemGroup.TOOLS));
+	//must figure out the Al axe and hoe (protected)
 
 
 	@Override
@@ -43,16 +29,8 @@ public class additionsMod implements ModInitializer {
 		System.out.println("AdditionsMC is initializing. Have fun!");
 
 		Registry.register(Registry.ITEM, new Identifier("additions", "aluminium_ingot"), ALUMINIUM_INGOT); //Al ingot
-		Registry.register(Registry.BLOCK, new Identifier("additions", "aluminium_ore"), ALUMINIUM_ORE); //Al ore
-
-		Registry.register(Registry.ITEM, new Identifier("additions", "silicon_ingot"), SILICON_INGOT); //Si Ingot
-		Registry.register(Registry.ITEM, new Identifier("additions", "silicon_plate"), SILICON_PLATE); //Si plate
 
 
-	/*@Override
-	RegistryKey<ConfiguredFeature<?, ?>> aluminium_ore = RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN,
-        new Identifier("additions", "aluminium_ore"));
-    Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, oreWoolOverworld.getValue(), ALUMINIUM_ORE);
-    BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, aluminium_ore);*/
+
 	}
 }
