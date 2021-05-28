ce7dd07b7704eaeb5b5761f215d95e3499912954
AV3_08 <brandonjoash@gmail.com>
2021-05-28 14:04:58 +0800

    modified plasma cannon states; changed mixin name.

--- src/main/java/net/pocostudios/additions/mixin/AdditionsMixin.java
+++ src/main/java/net/pocostudios/additions/mixin/AdditionsMixin.java
@@ -6,8 +6,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
-public class ExampleMixin {
+@org.spongepowered.asm.mixin.Mixin(TitleScreen.class)
+public class AdditionsMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
 		System.out.println("");
