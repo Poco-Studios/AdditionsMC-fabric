b66c5ddda4161a919114ed256869921ab2f740d8
AV3_08 <brandonjoash@gmail.com>
2021-07-04 21:44:29 +0800

    Update AdditionsMixin.java

--- src/main/java/net/pocostudios/additions/mixin/AdditionsMixin.java
+++ src/main/java/net/pocostudios/additions/mixin/AdditionsMixin.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @org.spongepowered.asm.mixin.Mixin(TitleScreen.class)
+
 public class AdditionsMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
